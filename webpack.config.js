0a2659ca5daf550df2ba347de6545dbcd4b1d7c6
Yu Qian <yuqian.124@gamil.com.com>
2019-09-25 00:19:34 +0800

    feat: 创建项目

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'phaser3-start',
+        template: 'index.html'
+      })]
+};
